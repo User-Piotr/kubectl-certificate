cccf02cd4179044790d19632c4fac2811d96fd93
Subzor <dancios25@gmail.com>
2024-11-18 15:43:14 +0100

    Initial commit with some code review

--- src/certificate/certs.py
+++ src/certificate/certs.py
@@ -1,7 +1,7 @@
 import base64
 
-from certificate.data import Certificate
-from certificate.data import Parameters
+from src.certificate.data import Certificate
+from src.certificate.data import Parameters
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from rich.console import Console
@@ -11,7 +11,11 @@ console = Console()
 
 class CertificateInfo:
 
-    def __extractr_cert_details(cert, verbose: bool) -> Certificate:
+    # dlaczego tutaj jest cert, a nie self jako pierwsze? Ewentualnie dlaczego nie jest to metoda statyczna?
+
+    # Zresztą czy nie lepiej jakby to było w klasie Certificate? Lub czy to w ogóle musi być klasą?
+    @staticmethod
+    def __extractr_cert_details(cert: x509.Certificate, verbose: bool) -> Certificate:
         """
         Extract the certificate details.
         """
@@ -46,14 +50,18 @@ class CertificateInfo:
         """
 
         # Load the certificate.
-        with open(parameters.cert_path, "rb") as cert_file:
-            cert_data = cert_file.read()
-            cert = x509.load_pem_x509_certificate(cert_data, default_backend())
+        try:
+            with open(parameters.cert_path, "rb") as cert_file:
+                cert_data = cert_file.read()
+                cert = x509.load_pem_x509_certificate(cert_data, default_backend())
+        except FileNotFoundError as error:
+            raise FileNotFoundError(f"File not found: {error.filename}")
+            #Ewentualnie można zrobić inną obsługę żeby program się nie kończył tylko zwracał None
 
         return CertificateInfo.__extractr_cert_details(cert, parameters.verbose)
 
     @staticmethod
-    def load_certificate_string(cert_data, parameters: Parameters) -> Certificate:
+    def load_certificate_string(cert_data: bytes, parameters: Parameters) -> Certificate:
         """
         Load a certificate from a PEM-encoded string.
         """
